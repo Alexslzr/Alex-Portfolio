25656c8a30c1b0e8bdf0fc9d19cbd446895427ca
alexslzr <alejandroslzr17@gmail.com>
2023-08-01 15:50:03 -0600

    added description to each project

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -7,7 +7,8 @@ const projects = [
                         app: 'https://sales-tracker-t4p2-f41e20adc513.herokuapp.com/login',
                         repo: 'https://github.com/Morkendi/Sales-Tracker',
                         tech: 'Js/NodeJs/ExpressJs/Mysql/TailwindCss',
-                        img: 'https://github.com/Morkendi/Sales-Tracker/blob/main/public/images/deployed.gif?raw=true'
+                        img: 'https://github.com/Morkendi/Sales-Tracker/blob/main/public/images/deployed.gif?raw=true',
+                        description: 'Fullstack Application for Sales employees, so they can easily monitor their earnings, add new sales, access their sales history, view product information, and obtain a summary of their sales performance.'
                     },
                     {
                         id: 2,
@@ -15,7 +16,8 @@ const projects = [
                         app: 'https://pacific-bayou-28721-544ed386ada5.herokuapp.com/',
                         repo: 'https://github.com/Alexslzr/Tech-Blog',
                         tech: 'Js/NodeJs/ExpressJs/Mysql/BootstrapCss',
-                        img:  'https://github.com/Alexslzr/Tech-Blog/raw/main/Assets/techblog.png'
+                        img:  'https://github.com/Alexslzr/Tech-Blog/raw/main/Assets/techblog.png',
+                        description: 'CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well. using the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.'
                     },
                     {
                         id: 3,
@@ -23,7 +25,8 @@ const projects = [
                         app: 'https://enigmatic-fjord-82076.herokuapp.com/notes',
                         repo: 'https://github.com/Alexslzr/Note-Taker',
                         tech: 'ExpressJs/Js/NodeJs',
-                        img: 'https://github.com/Alexslzr/Note-Taker/raw/main/Assets/11-express-homework-demo-01.png'
+                        img: 'https://github.com/Alexslzr/Note-Taker/raw/main/Assets/11-express-homework-demo-01.png',
+                        description: 'Note taker App where you can create , update and delete notes. This application use an Express.js back end and save and retrieve note data from a JSON file.'
                     }
                 ]
     },
@@ -35,7 +38,8 @@ const projects = [
                         app: 'https://github.com/Alexslzr/Employee-Tracker',
                         repo: 'https://github.com/Alexslzr/Employee-Tracker',
                         tech: 'Mysql/NodeJs/ExpressJs',
-                        img: 'https://github.com/Alexslzr/Employee-Tracker/raw/main/Assets/employeetracker.png'
+                        img: 'https://github.com/Alexslzr/Employee-Tracker/raw/main/Assets/employeetracker.png',
+                        description: 'Command-line application from scratch to manage a company\'s employee database, using Node.js, Inquirer, and MySQL.'
                     },
                     {
                         id: 5,
@@ -43,7 +47,8 @@ const projects = [
                         app: 'https://github.com/Alexslzr/E-Commerce-Back-End',
                         repo: 'https://github.com/Alexslzr/E-Commerce-Back-End',
                         tech: 'Mysql/NodeJs/ExpressJs/Sequalize',
-                        img: 'https://github.com/Alexslzr/E-Commerce-Back-End/raw/main/Assets/13-orm-homework-demo-01.gif'
+                        img: 'https://github.com/Alexslzr/E-Commerce-Back-End/raw/main/Assets/13-orm-homework-demo-01.gif',
+                        description: 'Back end for an e-commerce site. working with Express.js API and Sequelize to interact with a MySQL database.'
                     },
                     {
                         id: 6,
@@ -51,7 +56,8 @@ const projects = [
                         app: 'https://github.com/Alexslzr/README-Generator',
                         repo: 'https://github.com/Alexslzr/README-Generator',
                         tech: 'Mysql/NodeJs/ExpressJs',
-                        img: "https://github.com/Alexslzr/README-Generator/raw/main/assets/Readmegenerator.png"
+                        img: "https://github.com/Alexslzr/README-Generator/raw/main/assets/Readmegenerator.png",
+                        description: 'Command-line application intended to quickly and easily create a README file. This allows the project creator to devote more time to working on the project.'
                     }
                 ]
     },
@@ -63,7 +69,8 @@ const projects = [
                         app: 'https://github.com/Alexslzr/SVG-Logo-Maker',
                         repo: 'https://github.com/Alexslzr/SVG-Logo-Maker',
                         tech: 'NodeJs/ExpressJs',
-                        img: 'https://github.com/Alexslzr/SVG-Logo-Maker/raw/main/Images/10-oop-homework-demo.png'
+                        img: 'https://github.com/Alexslzr/SVG-Logo-Maker/raw/main/Images/10-oop-homework-demo.png',
+                        description: 'Command-line application that takes in user input to generate a logo and save it as an SVG file. The application prompts the user to select a color and shape, provide text for the logo, and save the generated SVG to a .svg file.'
 
                     },
                     {
@@ -72,15 +79,17 @@ const projects = [
                         app: 'https://alexslzr.github.io/Weather-Dashboard/Develop/',
                         repo: 'https://github.com/Alexslzr/Weather-Dashboard',
                         tech: 'Js/HTML/TailwindCss/Third-party-API',
-                        img: 'https://github.com/Alexslzr/Weather-Dashboard/raw/main/Develop/Assets/gif/weather.png'
+                        img: 'https://github.com/Alexslzr/Weather-Dashboard/raw/main/Develop/Assets/gif/weather.png',
+                        description: 'Weather Forecast app with the help of API displays the current weather and weather at same time for the next 5 days. uses local storage to save previous searches and displays them again by clicking on them'
                     },
                     {
-                        id: 11,
+                        id: 9,
                         title: 'Work Day Scheduler',
                         app: 'https://alexslzr.github.io/Work-Day-Scheduler/Develop/',
                         repo: 'https://github.com/Alexslzr/Work-Day-Scheduler',
                         tech: 'Jquery/JS/HTML/CSS',
-                        img: 'https://github.com/Alexslzr/Work-Day-Scheduler/raw/main/Develop/Assets/gif/05-third-party-apis-homework-demo.gif'
+                        img: 'https://github.com/Alexslzr/Work-Day-Scheduler/raw/main/Develop/Assets/gif/05-third-party-apis-homework-demo.gif',
+                        description: 'Calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.'
                     }
                 ]
     },{
@@ -91,23 +100,26 @@ const projects = [
                 app: 'https://alexslzr.github.io/CodeQuizz/Develop/',
                 repo: 'https://github.com/Alexslzr/CodeQuizz',
                 tech: 'HTML/CSS/JS',
-                img: 'https://github.com/Alexslzr/CodeQuizz/raw/main/Develop/Assets/gif/04-web-apis-homework-demo.gif'
+                img: 'https://github.com/Alexslzr/CodeQuizz/raw/main/Develop/Assets/gif/04-web-apis-homework-demo.gif',
+                description: 'Quiz Application with timer, the score would be the timer caught at the end of the quizz, and also a window for Highest scores which would be stored on local storage. for every wrong answer 10 seconds would be substracted from the timer'
             },
             {
-                id: 9,
+                id: 11,
                 title: 'Cine Tracker',
                 app: 'https://rponce23.github.io/Project_1/',
                 repo: 'https://github.com/rponce23/Project_1',
                 tech: 'Js/HTML/TailwindCss/Third-party-API',
-                img: 'https://github.com/rponce23/Project_1/raw/main/assets/images/Main%20Screen%20page.jpg'
+                img: 'https://github.com/rponce23/Project_1/raw/main/assets/images/Main%20Screen%20page.jpg',
+                description: 'Application which finds cinemas in a radious of 4km and the links of the respective sites of each of them, also displays a few available movies with their description'
             },
             {
-                id: 10,
+                id: 12,
                 title: "Social-Network-API",
                 app: 'https://github.com/Alexslzr/Social-Network-API',
                 repo: 'https://github.com/Alexslzr/Social-Network-API',
                 tech: "MongoDB/ExpressJs/Mongoose/NodeJs",
-                img: "https://github.com/Alexslzr/Social-Network-API/raw/main/Assets/18-nosql-homework-demo-01.gif"
+                img: "https://github.com/Alexslzr/Social-Network-API/raw/main/Assets/18-nosql-homework-demo-01.gif",
+                description: 'API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. It use Express.js for routing, a MongoDB database, and the Mongoose ODM.'
             }
            
         ]
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -15,16 +15,17 @@ export default function Portfolio({projects}) {
             <div className="d-flex justify-content-center m-5">
               {project.section.map((pro)=> (
                 <div className="project my-2 mx-4 p-4 rounded">
-                  <div className='d-flex align-items-center'>
+                  <div className='d-flex align-items-center uwu'>
                     <h1 key={pro.id}>{pro.title}</h1>
                     <a href={pro.repo} target="_blank">
                           <img style={styles.image} src={require("../../Images/github-mark-white.png")} alt="github-logo"></img>
                     </a>
                   </div>
                   <h5>{pro.tech}</h5>
-                  <a href={pro.app} target='_blank'>
+                  <a href={pro.app} target='_blank' className='tech'>
                     <img src={pro.img} alt={pro.title}></img>
                   </a>
+                  <p className='description'>{pro.description}</p>
                 </div>
               ))}
             </div> 
