8417389d57a8dd15210aa19af4ffd17b44955d92
alexslzr <alejandroslzr17@gmail.com>
2023-08-01 19:11:27 -0600

    updated aboutme text

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,10 +8,10 @@ export default function AboutMe() {
       <div className='mx-5 my-2 rounded p-3 about'>
         <h1 className='text-center'>About Me</h1>
         <p className='text-justify'>
-          I am Mechatronics Engineer with interest in software development,
-          on my free times i study by myself and i am currently taking a Fullstack development bootcamp 
+          I am Fullstack Web developer, on my free times i study by myself and i am currently taking a Fullstack development bootcamp 
           in order to improve my programing skills, what i also enjoy is hanging out with friends, go outside for a walk and play sports,
-          my favorite is soccer which i play since i was 5, my favorite color is green and my favorite food is tacos de carne asada.
+          i have a Bachelor of Science in Mechatronics Engineering, and my favorite sport and activity is soccer which i play since i was 5, 
+          my favorite color is green and i enjoy a lot eating tacos.
         </p>
       </div>
     </div>
